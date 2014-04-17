474b6c74dd5b2af80245fba85b29afda5e6866cb
nuomi <newchar@gmail.com>
2014-04-17 19:10:35 +0800

    remote api debug

--- cserver.coffee
+++ cserver.coffee
@@ -14,12 +14,17 @@ ping_get_pid = (hostname) ->
     req = http.request opt_ping, (res) ->
         res.on 'data',(chunk) ->
             console.log 'Response: \n'+chunk
-    req.write '&url=' + hostname + '&curl=&rt=1&nocache=0&host=&referer=&cookie=&agent=&speed=&pingcount=&pingsize=&area[]=0&area[]=1&area[]=2&area[]=3&&isp[]=0&isp[]=1&isp[]=2&isp[]=6&isp[]=7&isp[]=8&isp[]=4&' 
+
+    
+    load = '&url=' + hostname + '&curl=&rt=1&nocache=0&host=&referer=&cookie=&agent=&speed=&pingcount=&pingsize=&area[]=0&area[]=1&area[]=2&area[]=3&&isp[]=0&isp[]=1&isp[]=2&isp[]=6&isp[]=7&isp[]=8&isp[]=4&' 
+    console.log load
+    req.write load
     req.end()
 
 
 srv = http.createServer (req,res) ->
-    hostname = req.url[(req.url.indexOf '?' +1)..]
+    hostname = req.url.substring req.url.indexOf('?')+1, req.url.len
+    console.log hostname
     if req.method is 'GET' and (/^\/ping/).test req.url
         console.log 'in trap'
         ping_get_pid hostname
--- run.py
+++ run.py
@@ -1,8 +1,46 @@
 #!/usr/bin/env python
 
 import sys
+import requests, json
+import urllib
+
+site_http_payload = {
+        'curl':'',
+        'rt':'1',
+        'nocache':'0',
+        'host':'',
+        'referer':'',
+        'cookie':'',
+        'agent':'',
+        'speed':'',
+        'pingcount':'',
+        'pingsize':'',
+        'area[]':'0',
+        'area[]':'2',
+        'area[]':'3',
+        'area[]':'1',
+        'isp[]':'0',
+        'isp[]':'1',
+        'isp[]':'2',
+        'isp[]':'6',
+        'isp[]':'7',
+        'isp[]':'8',
+        'isp[]':'4',
+}
+
+site_http_headers = {
+    'Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:20.0) Gecko/20100101 Firefox/20.0',
+    'Referer':'http://www.17ce.com/'
+}
+
+def site_http(target):
+    payload = site_http_payload
+    payload['url'] = urllib.quote(target)
+    print payload
+    ret = requests.post('http://www.17ce.com/site/http', data = payload, headers = site_http_headers)
+    print ret
+
 
 if __name__ == '__main__':
-
     for arg in sys.argv[1:]:
-        print arg
+        site_http(arg)
